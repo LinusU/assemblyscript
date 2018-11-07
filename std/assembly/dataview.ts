a786204f995e7b731c5a0ffd38d22d48f8c52062
Linus Unnebäck <linus@folkdatorn.se>
2018-11-07 19:16:50 +0000

    [WIP] Implement all get* functions

--- std/assembly/dataview.ts
+++ std/assembly/dataview.ts
@@ -1,49 +1,57 @@
-import {
-  loadUnsafeWithOffset,
-} from "./internal/arraybuffer";
+import { HEADER_SIZE } from "./internal/arraybuffer";
+
+@inline
+function get<T> (buffer: ArrayBuffer, byteOffset: i32, littleEndian: boolean): T {
+  const result = load<T>(changetype<usize>(buffer) + byteOffset, HEADER_SIZE);
+  return littleEndian ? result : bswap<T>(result);
+}
 
 export class DataView {
-  readonly buffer: ArrayBuffer;
-  readonly byteOffset: i32;
-  readonly byteLength: i32;
-
-  constructor (buffer: ArrayBuffer, byteOffset: i32, byteLength: i32) {
-    this.buffer = buffer
-    this.byteOffset = byteOffset
-    this.byteLength = byteLength
+  constructor(
+    readonly buffer: ArrayBuffer,
+    readonly byteOffset: i32 = 0,
+    readonly byteLength: i32 = i32.MAX_VALUE,
+  ) {
+    this.byteLength = min(buffer.byteLength, byteLength);
   }
 
   @inline
-  getUint32 (byteOffset: u32, littleEndian: boolean): u32 {
-    if (littleEndian) {
-      return (
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 0, this.byteOffset) << 24) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 1, this.byteOffset) << 16) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 2, this.byteOffset) << 8) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 3, this.byteOffset) << 0)
-      );
-    } else {
-      return (
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 0, this.byteOffset) << 0) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 1, this.byteOffset) << 8) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 2, this.byteOffset) << 16) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 3, this.byteOffset) << 24)
-      );
-    }
+  getFloat32 (byteOffset: i32, littleEndian: boolean): f32 {
+    return get<f32>(this.buffer, byteOffset, littleEndian);
   }
 
   @inline
-  getUint16 (byteOffset: u32, littleEndian: boolean): u16 {
-    if (littleEndian) {
-      return (
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 0, this.byteOffset) << 8) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 1, this.byteOffset) << 0)
-      );
-    } else {
-      return (
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 0, this.byteOffset) << 0) &
-        (loadUnsafeWithOffset<u8, u8>(this.buffer, byteOffset + 1, this.byteOffset) << 8)
-      );
-    }
+  getFloat64 (byteOffset: i32, littleEndian: boolean): f64 {
+    return get<f64>(this.buffer, byteOffset, littleEndian);
+  }
+
+  @inline
+  getInt8 (byteOffset: i32, littleEndian: boolean): i8 {
+    return get<i8>(this.buffer, byteOffset, littleEndian);
+  }
+
+  @inline
+  getInt16 (byteOffset: i32, littleEndian: boolean): i16 {
+    return get<i16>(this.buffer, byteOffset, littleEndian);
+  }
+
+  @inline
+  getInt32 (byteOffset: i32, littleEndian: boolean): i32 {
+    return get<i32>(this.buffer, byteOffset, littleEndian);
+  }
+
+  @inline
+  getUint8 (byteOffset: i32, littleEndian: boolean): u8 {
+    return get<u8>(this.buffer, byteOffset, littleEndian);
+  }
+
+  @inline
+  getUint16 (byteOffset: i32, littleEndian: boolean): u16 {
+    return get<u16>(this.buffer, byteOffset, littleEndian);
+  }
+
+  @inline
+  getUint32 (byteOffset: i32, littleEndian: boolean): u32 {
+    return get<u32>(this.buffer, byteOffset, littleEndian);
   }
 }
